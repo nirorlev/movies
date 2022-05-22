011e662c48632a9116e1096c27e73de75abe188e
Cristian Ribeiro <fhr.cristian@gmail.com>
2022-05-22 11:16:38 -0300

    TAKS 1 - remove redux scripts

--- src/App.js
+++ src/App.js
@@ -1,28 +1,11 @@
-import React, { Component } from 'react'
-import {createStore, applyMiddleware, compose} from 'redux'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
+import React, { Component } from "react";
 
-import rootReducer from './rootReducer'
-import MovieLibrary from './MovieLibrary'
-
-
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
-
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-)
-
+import MovieLibrary from "./MovieLibrary";
 
 class App extends Component {
   render() {
-    return (
-      <Provider store={store}>
-        <MovieLibrary />
-      </Provider>
-    )
+    return <MovieLibrary />;
   }
 }
 
-export default App
+export default App;
