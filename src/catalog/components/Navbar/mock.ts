3efe582c1582b65951c1c83881f70808c0e6de79
Cristian Ribeiro <fhr.cristian@gmail.com>
2022-05-23 20:27:59 -0300

    TAKS 1 - prevent relative path

--- src/catalog/components/Navbar/mock.ts
+++ src/catalog/components/Navbar/mock.ts
@@ -2,13 +2,17 @@ import { DeviceData } from "./type";
 
 const data: { desktop: DeviceData; mobile: DeviceData } = {
   desktop: {
-    '/': "Home",
-    "tv-shows": "Tv Shows",
-    movies: "Movies",
-    top: "Top",
-    "my-list": "My List",
+    "/home": "Home",
+    "/tv-shows": "Tv Shows",
+    "/movies": "Movies",
+    "/top": "Top",
+    "/my-list": "My List",
   },
-  mobile: { "tv-shows": "Tv Shows", movies: "Movies", "my-list": "My List" },
+  mobile: {
+    "/tv-shows": "Tv Shows",
+    "/movies": "Movies",
+    "/my-list": "My List",
+  },
 };
 
 export default data;
