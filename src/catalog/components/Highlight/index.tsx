d69784dba714f4b2f52ef49bc23c66c3fe118004
Cristian Ribeiro <fhr.cristian@gmail.com>
2022-05-24 22:25:25 -0300

    TAKS 1 - add responsiveness to highlight

--- src/catalog/components/Highlight/index.tsx
+++ src/catalog/components/Highlight/index.tsx
@@ -1,18 +1,21 @@
 import React from 'react';
-import { Button } from '../../../commons';
+import { Button, useDevice } from '../../../commons';
 import { MockData } from '../../../mocks';
 import './styles.scss';
 
 type Props = { info?: MockData, play?: React.ReactNode, more?: React.ReactNode }
 
 export const Highlight: React.FC<Props> = ({ info, play, more }) => {
+  const { isxs } = useDevice();
+
   return (
     <div className='highlight'>
+      {isxs && <span className='highlight__text'>netflix</span> }
       {info && info.title && <span className='highlight__title'>{info.title}</span>}
-      {info && info.overview && <span className='highlight__description'>{info.overview}</span>}
+      {!isxs && info && info.overview && <span className='highlight__description'>{info.overview}</span>}
       <div className='highlight__buttons'>
         <Button text='Play' type='primary' icon={play} />
-        <Button text='More' type='secondary' icon={more} />
+        {!isxs && <Button text='More' type='secondary' icon={more} />}
       </div>
     </div>
   );
